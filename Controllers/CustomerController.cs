f5c0bab6209efbe560e678b5c7593ca800a233f3
ihebbg <ibengara7@gmail.com>
2026-07-02 12:43:14 +0200

    maj

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 namespace BankAccountServices.Controllers
 {
-	[Authorize]
+	//[Authorize]
 	[Route("api/[controller]")]
 	[ApiController]
 	public class CustomerController(ICustomerService customerService) : ControllerBase
