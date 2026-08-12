601bdc71419565644ec21ac08388c1c10de4b023
ihebbg <ibengara7@gmail.com>
2026-08-12 16:13:46 +0200

    maj

--- Controllers/OperationController.cs
+++ Controllers/OperationController.cs
@@ -18,7 +18,7 @@ namespace BankAccountServices.Controllers
 		[HttpGet("hello-world")]
 		public ActionResult<List<OperationResponseDTO>> HelloWorld()
 		{
-			throw new Exception("loljjj");
+			throw new Exception("loljjjd");
 		}
 
 		/// <summary>
