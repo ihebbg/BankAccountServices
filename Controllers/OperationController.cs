aaff29a1168800320980b0270f45f6727991122a
ihebbg <ibengara7@gmail.com>
2026-08-13 14:47:31 +0200

    maj

--- Controllers/OperationController.cs
+++ Controllers/OperationController.cs
@@ -18,7 +18,7 @@ namespace BankAccountServices.Controllers
 		[HttpGet("hello-world")]
 		public ActionResult<List<OperationResponseDTO>> HelloWorld()
 		{
-			throw new Exception("hellogf an error");
+			throw new Exception("hellogf dan error");
 		}
 
 		/// <summary>
